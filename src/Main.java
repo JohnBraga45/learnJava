a80b0bcfc0e86100723c92b7589e38bcc24ac559
Dionísio Braga <jhonibraga45@gmail.com>
2024-01-25 09:37:53 +0000

    Primeiros passos no Java

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        var welcome = "Seja Bem-Vindo ";
+        var user = "John";
+
+        System.out.println(welcome + user);
+
+
+
+    }
+}
