ed250d27428c7f19030adc4959eed3084cef89e8
Dionísio Braga <jhonibraga45@gmail.com>
2024-01-25 16:17:43 +0000

    mini calculadora usando entrada de dados

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 import java.util.Random;
+import java.util.Scanner;
 
-  public class Main {
+public class Main {
     public static void main(String[] args) {
-        Random generate = new Random();
+//        Random generate = new Random();
 //        Usando while
 //        int i =0;
 //        while (i < 6 ){
@@ -13,11 +14,24 @@ import java.util.Random;
 //
 //        }
 
-        for (int i = 0; i < 6; i++ ){
-        int number = generate.nextInt(10);
-            System.out.println(number);
-        }
-        }
+//        for (int i = 0; i < 6; i++ ){
+//        int number = generate.nextInt(10);
+//            System.out.println(number);
+//        }
+
+        //mini calculadora
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Digite o num1: ");
+//        int num1 = scanner.nextInt();
+//
+//        System.out.println("Digite o num2: ");
+//        int num2 = scanner.nextInt();
+//
+//        System.out.println("A soma é: "+ (num1+num2));
+//        System.out.println("A subtração é: "+ (num1-num2));
+//        System.out.println("A multiplicação é: "+ (num1*num2));
+    }
 
 
     }
